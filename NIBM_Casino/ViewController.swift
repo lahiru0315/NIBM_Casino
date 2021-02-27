7bf7b201b4d1f9bbc651c7a97ed2df8e5db82e72
lahiru0315 <lahiruekanayake2@gmail.com>
2021-02-26 23:50:01 -0800

    initial commit

---
+++ NIBM_Casino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NIBM_Casino
+//
+//  Created by Lahiru on 2/26/21.
+//  Copyright © 2021 Lahiru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    @IBOutlet weak var imageViewDice1: UIImageView!
+    override func viewDidLoad() {
+        imageViewDice1.image=#imageLiteral(resourceName: "dicethree")
+    
+    }
+}
+
